eb44d0e3de081db4d2af9dea675a97c84453a872
bharathi-selvakumar <bharathiselvakumar286@gmail.com>
2025-02-09 21:43:01 +0530

    update0.1

--- src/MuiDesigns/useStepper.js
+++ src/MuiDesigns/useStepper.js
@@ -4,10 +4,6 @@ import { useState } from "react";
 export const useStepper = () => {
 
   const totalSteps = 7;
-<<<<<<< HEAD
-  console.log(totalSteps)
-=======
->>>>>>> 9b7212e (Updated_PDF)
   const [activeStep, setActiveStep] = useState(0);
   const handleNext = () => {
     if (activeStep < totalSteps - 1) {
